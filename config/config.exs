9b25914f93b4fd8f2eadde02ebb0ca9166de7d71
twozeronine <amdusias209@gmail.com>
2023-03-22 23:51:23 +0900

    feat : Set first installation

--- config/config.exs
+++ config/config.exs
@@ -1,23 +1,14 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 import Config
 
 config :my_oban_phoenix,
   ecto_repos: [MyObanPhoenix.Repo]
 
-# Configures the endpoint
 config :my_oban_phoenix, MyObanPhoenixWeb.Endpoint,
   url: [host: "localhost"],
   render_errors: [view: MyObanPhoenixWeb.ErrorView, accepts: ~w(html json), layout: false],
   pubsub_server: MyObanPhoenix.PubSub,
   live_view: [signing_salt: "8vKh0oCx"]
 
-# Configure esbuild (the version is required)
 config :esbuild,
   version: "0.14.29",
   default: [
@@ -27,14 +18,15 @@ config :esbuild,
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
+config :my_oban_phoenix, Oban,
+  repo: MyObanPhoenix.Repo,
+  plugins: [Oban.Plugins.Pruner],
+  queues: [default: 10]
+
 import_config "#{config_env()}.exs"
--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,5 @@
 import Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :my_oban_phoenix, MyObanPhoenix.Repo,
   username: "postgres",
   password: "postgres",
@@ -13,15 +8,12 @@ config :my_oban_phoenix, MyObanPhoenix.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :my_oban_phoenix, MyObanPhoenixWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "j9QgAVTjATRytp3QufWfUA90sh0ohT7VHebSRptmUQrhP75YaWocJINtde3r6RdH",
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warn
-
-# Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+config :my_oban_phoenix, Oban, testing: :inline
