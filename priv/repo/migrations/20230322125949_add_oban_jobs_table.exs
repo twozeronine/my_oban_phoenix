060406b5ff924689a47a4093bd328248e264c2fe
twozeronine <amdusias209@gmail.com>
2023-03-22 23:33:45 +0900

    feat : 잘못된거 수정

--- priv/repo/migrations/20230322125949_add_oban_jobs_table.exs
+++ priv/repo/migrations/20230322125949_add_oban_jobs_table.exs
@@ -1,7 +1,11 @@
 defmodule MyObanPhoenix.Repo.Migrations.AddObanJobsTable do
   use Ecto.Migration
 
-  def change do
+  def up do
+    Oban.Migration.up(version: 11)
+  end
 
+  def down do
+    Oban.Migration.down(version: 1)
   end
 end
